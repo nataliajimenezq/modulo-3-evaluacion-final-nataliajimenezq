3ab6db81905b54c5f018034001f2ba0a122b22c4
nataliajimenezq <snjimenezq@gmail.com>
2020-01-15 16:18:57 +0100

    Erase default React Info

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../stylesheet/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Probando...
+      </div>
+    );
+  }
+}
+
+export default App;
