568b9817f0a698dd68fdce63d980050818c9e636
nataliajimenezq <snjimenezq@gmail.com>
2020-01-16 13:14:54 +0100

    Add episode feature to Detail Component

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -3,17 +3,15 @@ import {Link} from 'react-router-dom';
 
   const CharacterDetail = props => {
 
-
     return (
         <div className="card__container">
           <img src={props.singleCharacter.image} alt={`${props.singleCharacter.name} avatar`} />
-
             <h2>{props.singleCharacter.name}</h2>
             <div>
             <span>Status:{props.singleCharacter.status}</span>
             <span>Species: {props.singleCharacter.species}</span>
-            {/* <span>Origin: {props.singleCharacter.origin}</span> */}
-            {/* <span>Episodes: {props.singleCharacter.episodes}</span>  */}
+            <span>Origin: {props.singleCharacter.origin.name}</span>
+            <span>Episodes: {props.singleCharacter.episode.length}</span>  
             </div>
           
         </div>
